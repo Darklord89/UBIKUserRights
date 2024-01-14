bbbed0cafe99588eaa02c276c219720e3d61e172
Darklord89 <bestpyro@gmail.com>
2024-01-14 21:23:29 +0100

    Updated errors in compilation

---
+++ UBIKUserRights/SystemDefinitons/UserRightsSystemDefinitionProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UBIK.Data;
+
+namespace UBIKUserRights.SystemDefinitons
+{
+    internal class UserRightsSystemDefinitionProvider : SystemDefinitionProviderBase
+    {
+        public override Guid ModuleID => UBIKUserRightsPlugin.ID;
+
+        private static UserRightsSystemDefinitionProvider instance;
+
+        public static UserRightsSystemDefinitionProvider Instance => instance ?? (instance = new UserRightsSystemDefinitionProvider());
+
+        private UserRightsSystemDefinitionProvider()
+            : base(new ISystemDefinitions[]
+            {
+                
+            })
+        { }
+    }
+}
--- UBIKUserRights/UBIKUserRightsPlugin.cs
+++ UBIKUserRights/UBIKUserRightsPlugin.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using UBIK.Data;
 using UBIK.Injection;
 using UBIK.Kernel;
 using UBIK.Kernel.Classification;
+using UBIKUserRights.SystemDefinitons;
 
 namespace UBIKUserRights
 {
@@ -15,8 +18,32 @@ namespace UBIKUserRights
     [ExportMetadata("Company", "Siemens s.r.o.")]
     [ExportMetadata("Copyright", "2023, Siemens s.r.o.")]
     [ExportMetadata("MinimumKernelVersion", "3.6.0.0")]
-    public class UBIKUserRightsPlugin : IUbikPlugin
+    public class UBIKUserRightsPlugin : ISessionAwareUbikModule, IUbikPlugin
     {
-        
+        public static Guid ID => Guid.Parse("E3F0B5F5-DC5F-4D83-9FB4-ADFDD9CD400A");
+
+        public static UBIKEnvironment UBIKEnvironment { get; internal set; }
+
+        public ISystemDefinitionProvider SystemDefinitionsProvider => UserRightsSystemDefinitionProvider.Instance;
+
+        public void Initialize(UBIKEnvironment environment)
+        {
+            UBIKEnvironment = environment;
+        }
+
+        public bool Initialized() => UBIKEnvironment != null;
+
+        public void Terminate(UBIKEnvironment environment)
+        {
+            Terminate();
+        }
+
+        public void Terminate()
+        {
+            if ( UBIKEnvironment != null )
+            {
+                UBIKEnvironment = null;
+            }
+        }
     }
 }
